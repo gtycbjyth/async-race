620b3faa74a9ae56d3fb5040aca506cad9771841
gtycbjyth <kovaltru@gmail.com>
2021-06-12 22:31:18 +0300

    refactor: fix generate random color

--- src/helper/generateOneHundredCars .ts
+++ src/helper/generateOneHundredCars .ts
@@ -4,8 +4,8 @@ import { TCarParam } from '../model/types';
 const generateOneHundredCars = (): TCarParam[] => {
   const addTypeCar = carModel.map(function f(elem) {
     const type = carType[Math.floor(Math.random() * carType.length)];
-    const color = () => Math.floor(Math.random() * 256);
-    return { name: `${elem} ${type}`, color: `#${color()} ${color()} ${color()}` };
+    const color = () => Math.floor(Math.random() * 1000000);
+    return { name: `${elem} ${type}`, color: `#${color()}` };
   });
 
   const arrCars = new Array(100).fill('1');
