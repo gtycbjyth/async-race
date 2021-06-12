3413f77bcfa06366c12dd48cc79ab9f04f158e27
gtycbjyth <kovaltru@gmail.com>
2021-06-12 22:33:32 +0300

    refactor: event listeners in game

--- src/model/class/button.ts
+++ src/model/class/button.ts
@@ -1,6 +1,7 @@
-import { createCar, getCars } from '../../api/garage';
-import generateOneHundredCars from '../../helper/generateOneHundredCars ';
-import appNew from '../../index';
+// import { createCar, getCars } from '../../api/garage';
+// // import UIData from '../../data/UIData';
+// import generateOneHundredCars from '../../helper/generateOneHundredCars ';
+// import appNew from '../../index';
 import CreateHTMLElement from './createHTMLElement';
 
 class Button extends CreateHTMLElement {
@@ -8,53 +9,54 @@ class Button extends CreateHTMLElement {
     super('button', '', id, name);
     this.element.innerText = name;
     this.element.id = id;
-    this.element.addEventListener('click', (event) => this.onClick(event));
+    // this.element.addEventListener('click', (event) => this.onClick(event));
   }
 
-  garage(event: any): void {
-    const t = this.element;
-    event.target.classList.toggle('active');
-    // const cars = getCars();
-    // console.log(cars);
-    appNew.reRender();
-  }
-
-  winners(): void {
-    const t = this.element;
-    appNew.render();
-    console.log('winners metod');
-  }
-
-  // race(): void {
-  //   console.log('winners metod');
+  // async garage(event: any): Promise<void> {
+  //   const t = this.element;
+  //   await event.target.classList.toggle('active');
+  //   await appNew.reRender();
   // }
 
-  // reset(): void {
-  //   console.log('winners metod');
+  // winners(): void {
+  //   const t = this.element;
   //   appNew.render();
+  //   console.log('winners metod');
   // }
 
-  select(event: any) {
-    const id = event.target.id.stlit('_');
-    console.log(id);
-  }
+  // // race(): void {
+  // //   console.log('winners metod');
+  // // }
 
-  async generate() {
-    console.log(this.element);
-    const oneHundredCar = generateOneHundredCars();
-    oneHundredCar.forEach((el) => createCar(el));
-    await appNew.reRender();
-  }
+  // // reset(): void {
+  // //   console.log('winners metod');
+  // //   appNew.render();
+  // // }
 
-  onClick(event: MouseEvent): void {
-    const { id } = event.target;
-    const target = id.split('_');
-    console.log('onClick Target', id.split('_'));
+  // select(event: any) {
+  //   const id = event.target.id.stlit('_');
+  //   console.log(id);
+  // }
 
-    if (target) {
-      this[target[0]](event);
-    }
-  }
+  // async generate() {
+  //   console.log(this.element);
+  //   const oneHundredCar = generateOneHundredCars();
+  //   await oneHundredCar.forEach((el) => createCar(el));
+  //   await getCars();
+  //   // await appNew.raceInfo.reRender();
+  //   // await appNew.carArr.forEach((car) => car.reRender());
+  //   await appNew.reRender();
+  // }
+
+  // onClick(event: MouseEvent): void {
+  //   const { id } = event.target;
+  //   const target = id.split('_');
+  //   console.log('onClick Target', id.split('_'));
+
+  //   if (target) {
+  //     this[target[0]](event);
+  //   }
+  // }
 }
 
 export default Button;
