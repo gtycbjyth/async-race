d5f427e04163a76a12a13e5e5e0914e7386fef27
gtycbjyth <kovaltru@gmail.com>
2021-06-16 19:37:50 +0300

    feat: race statistic and animation

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import './sass/style.scss';
 import App from './component/app';
-import { getCars } from './api/garage';
 import inputUI from './helper/inputUI';
+import writeUIDate from './helper/writeUIDate';
 
 const appNew = new App();
 
 async function start(): Promise<void> {
-  await getCars();
+  await writeUIDate();
   await appNew.render();
-  await inputUI(appNew);
+  inputUI(appNew);
 }
 
 start();
