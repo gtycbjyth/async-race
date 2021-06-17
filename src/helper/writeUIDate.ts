a4188c3d237c3ef8f04fa6f4f39c4f0a744fe1c0
gtycbjyth <kovaltru@gmail.com>
2021-06-17 16:34:34 +0300

    refactor: deleted console.log

--- src/helper/writeUIDate.ts
+++ src/helper/writeUIDate.ts
@@ -1,22 +1,7 @@
 import { getCars } from '../api/garage';
-// import { createWinner, deleteWinner, getWinners } from '../api/winners';
-// import UIData from '../data/UIData';
 
 const writeUIDate = async (): Promise<void> => {
   await getCars('all');
   await getCars();
-  // const winners = await getWinners();
-  // winners.forEach(async (winner) => {
-  //   await deleteWinner(winner.id);
-  // });
-  // UIData.allCars.forEach(async (car) => {
-  //   const winner = {
-  //     id: car.id,
-  //     wins: 0,
-  //     time: 0,
-  //     name: car.name,
-  //   };
-  //   await createWinner(winner);
-  // });
 };
 export default writeUIDate;
--- src/model/class/button.ts
+++ src/model/class/button.ts
@@ -5,54 +5,7 @@ class Button extends CreateHTMLElement {
     super('button', '', id, name);
     this.element.innerText = name;
     this.element.id = id;
-    // this.element.addEventListener('click', (event) => this.onClick(event));
   }
-
-  // async garage(event: any): Promise<void> {
-  //   const t = this.element;
-  //   await event.target.classList.toggle('active');
-  //   await appNew.reRender();
-  // }
-
-  // winners(): void {
-  //   const t = this.element;
-  //   appNew.render();
-  //   console.log('winners metod');
-  // }
-
-  // // race(): void {
-  // //   console.log('winners metod');
-  // // }
-
-  // // reset(): void {
-  // //   console.log('winners metod');
-  // //   appNew.render();
-  // // }
-
-  // select(event: any) {
-  //   const id = event.target.id.stlit('_');
-  //   console.log(id);
-  // }
-
-  // async generate() {
-  //   console.log(this.element);
-  //   const oneHundredCar = generateOneHundredCars();
-  //   await oneHundredCar.forEach((el) => createCar(el));
-  //   await getCars();
-  //   // await appNew.raceInfo.reRender();
-  //   // await appNew.carArr.forEach((car) => car.reRender());
-  //   await appNew.reRender();
-
-  // }
-
-  // onClick(event: MouseEvent): void {
-  //   const { id } = event.target;
-  //   const target = id.split('_');
-
-  //   if (target) {
-  //     this[target[0]](event);
-  //   }
-  // }
 }
 
 export default Button;
